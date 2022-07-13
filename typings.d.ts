3f5239495ffb5a6e868f1411e570d65706ce5a70
Dimitre Ortt <dimiortt@gmail.com>
2022-07-13 02:58:45 -0300

    Created header

---
+++ typings.d.ts
@@ -0,0 +1,19 @@
+export type Post = {
+  _id: string;
+  _createdAt: string;
+  _title: string;
+  author: {
+    name: string;
+    image: string;
+  };
+  description: string;
+  mainImage: {
+    asset: {
+      url: string;
+    };
+  };
+  slug: {
+    current: string;
+  };
+  body: [object];
+};
