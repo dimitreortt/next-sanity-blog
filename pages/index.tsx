2819107b4f4ecebe75b35147169431858877fe60
Dimitre Ortt <dimiortt@gmail.comt>
2022-07-28 14:45:03 -0300

    Added sort posts by title

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,31 +39,36 @@ const Home: NextPage<Props> = ({ posts }) => {
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md-gap-6 p-2 md:p-6">
-        {posts.map((post) => (
-          <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="border rounded-lg group cursor-pointer hover:scale-105">
-              <img
-                className="h-60 w-full object-cover  transition-transform duration-200 ease-in-out"
-                src={urlFor(post.mainImage).url()}
-                alt=""
-              />
-              <div className="flex justify-between p-5 bg-white ">
-                <div>
-                  <p className="text-lg font-bold">{post.title}</p>
-                  <p className="text-xs">
-                    {post.description} by{" "}
-                    <span className="font-bold">{post.author.name}</span>
-                  </p>
+        {posts
+          .sort((p1, p2) => (p1.title > p2.title ? 1 : -1))
+          .map((post) => {
+            console.log(post.title);
+            return (
+              <Link key={post._id} href={`/post/${post.slug.current}`}>
+                <div className="border rounded-lg group cursor-pointer hover:scale-105">
                   <img
-                    className="h-12 w-12 rounded-full"
-                    src={urlFor(post.author.image).url()}
+                    className="h-60 w-full object-cover  transition-transform duration-200 ease-in-out"
+                    src={urlFor(post.mainImage).url()}
                     alt=""
                   />
+                  <div className="flex justify-between p-5 bg-white ">
+                    <div>
+                      <p className="text-lg font-bold">{post.title}</p>
+                      <p className="text-xs">
+                        {post.description} by{" "}
+                        <span className="font-bold">{post.author.name}</span>
+                      </p>
+                      <img
+                        className="h-12 w-12 rounded-full"
+                        src={urlFor(post.author.image).url()}
+                        alt=""
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </Link>
-        ))}
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
